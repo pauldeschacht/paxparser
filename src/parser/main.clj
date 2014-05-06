284cc1498a8e02ef138b8cfa8c5136828d5781d9
Paul De Schacht <pauldeschacht@gmail.com>
2014-05-06 09:34:11 +0200

    test new ANAC format

--- src/parser/main.clj
+++ src/parser/main.clj
@@ -1,6 +1,7 @@
 (ns parser.main
   (:use [parser.pax.dsl.bts-db1b :as db1b])
+  (:use [parser.pax.dsl.anac-2 :as anac-2014])
   (:gen-class))
 
 (defn -main[]
-  (test-bts-db1b))
+  (test-anac-2014-core))

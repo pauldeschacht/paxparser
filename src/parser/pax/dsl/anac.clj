6dff0d2c2dbe8b5561baac4b5447fd49172e189a
Paul De Schacht <pauldeschacht@gmail.com>
2014-06-23 17:37:53 +0200

    update ANAC specifications

--- src/parser/pax/dsl/anac.clj
+++ src/parser/pax/dsl/anac.clj
@@ -6,13 +6,13 @@
 ;; ANAC
 ;;
 (defn anac-trim []
-  (fn [ctxt value]
+  (fn [ctxt value & line]
     (if (not (empty? value))
       (clojure.string/trim value)
       value)))
 
 (defn anac-convert-to-int []
-  (fn [specs value]
+  (fn [specs value & line]
     (if (empty? value)
       nil
       (let [value* (clojure.string/replace value #"[\.,]" "")]
@@ -22,7 +22,6 @@
                                  nil)))
         ))))
   
-
 (def anac-spec
   {:global {:thousand-separator "."
             :decimal-separator ""
@@ -52,95 +51,24 @@
    :output (generic-pax-output)
    })
 
-(def anac-spec-temp
-  {:global {:thousand-separator "."
-            :decimal-separator ""
-            :output-separator "\t"
-            }
-
-   :skip [(line-contains? ["Dependência" "Superintendência" "INFRAERO"])]
-   
-   :tokens [{:index 0 :name "icao_name" :split (split-into-cells ["origin-airport-icao" "origin-airport-name"] "-")}
-            {:index 4 :name "totdom"}
-            {:index 5 :name "totint"}
-            {:index 6 :name "tottot"}
-           ]
-   :columns [{:name "paxtype" :value "airport"}
-             {:name "paxsource" :value "ANAC"}
-             {:name "fullname" :transform (get-fullname)}
-             {:name "capture-date" :transform (get-capture-date)}
-             {:name "valid-from" :transform (get-valid-from)}
-             {:name "valid-to" :transform (get-valid-to)}
-             {:name "metric" :value "pax"}
-             {:name "segment" :value nil}
-             {:name "origin-aiport-icao" :transform (anac-trim)}
-             {:name "origin-airport-name" :transform (anac-trim)}
-             {:name "totdom" :transform (anac-convert-to-int)}
-             {:name "totint" :transform (anac-convert-to-int)}
-             {:name "tottot" :transform (anac-convert-to-int)}]
-   :output [{:name "paxtype" :value "airport"}
-            {:name "origin-airport-icao"}
-            {:name "origin-airport-iata"}
-            {:name "origin-airport-name"}
-            {:name "totdom"}
-            {:name "totint"}
-            {:name "tottot"}
-            {:name "transit" :value ""}]
-   })
-
-
 (defn test-anac []
-  (let [f1 "/home/pdeschacht/dev/paxparser/test/public-data/2014/02/ANAC/2013/12/ANAC.xlsx"
-        f2 "/home/pdeschacht/dev/paxparser/test/public-data/2014/02/ANAC/2013/12/test.csv"
-        sheetname "Sheet1"
-        file-info (extract-file-information f1)
-        specs (merge anac-spec {:global (merge (:global anac-spec) {:file-info file-info})})
-        specs* (add-defaults-to-specs specs)
-        params {:filename f1 :sheetname sheetname :max 200}
-        lines (read-lines params)
+  (let [in "/home/pdeschacht/dev/paxparser/test/public-data/2014/02/ANAC/2013/12/ANAC.xlsx"
+        out "/home/pdeschacht/dev/paxparser/test/public-data/2014/02/ANAC/2013/12/ANAC.csv"
+        sheet "Sheet1"
         ]
+    (convert-pax-file in anac-spec out sheet)
+    )
+  )
 
-    (->> lines
-         (wrap-text-lines)
-         (skip-lines (:skip specs*))
-         (remove-skip-lines)
-         (tokenize-lines (re-pattern (get-in specs* [:global :token-separator])))
-         (lines-to-cells (:tokens specs*))
-         (merge-lines-with-column-specs (:columns specs*))
-         (add-new-column-specs-lines (:columns specs*))
-         (transform-lines specs*)
-         (output-lines specs*)
-         (clean-outputs-lines)
-         (outputs-to-csv-lines (get-in specs* [:global :output-separator]))
-         (csv-outputs-to-file f2)
-         )
-    ))
-
-(defn test-anac-temp []
-  (let [f1 "/home/pdeschacht/MyDocuments/ANAC_201402.xlsx"
-        f2 "/home/pdeschacht/ANAC_201402.csv"
-        sheetname "Sheet1"
-        ;;file-info (extract-file-information f1)
-        file-info {}
-        specs (merge anac-spec-temp {:global (merge (:global anac-spec-temp) {:file-info file-info})})
-        specs* (add-defaults-to-specs specs)
-        params {:filename f1 :sheetname sheetname}
-        lines (read-lines params)
-        ]
-
-    (->> lines
-         (wrap-text-lines)
-         (skip-lines (:skip specs*))
-         (remove-skip-lines)
-         (tokenize-lines (re-pattern (get-in specs* [:global :token-separator])))
-         (lines-to-cells (:tokens specs*))
-         (merge-lines-with-column-specs (:columns specs*))
-         (add-new-column-specs-lines (:columns specs*))
-         (transform-lines specs*)
-         (output-lines specs*)
-         (clean-outputs-lines)
-         (outputs-to-csv-lines (get-in specs* [:global :output-separator]))
-         (csv-outputs-to-file f2)
-         )
-    ))
+(defn anac-all [folder]
+  (for [year (range 2012 2013)
+        month (range 1 13)]
+    (let [str-month (format "%02d" month)
+          in (str folder "/" year "/" str-month "/01_importAirport.xls")
+          out (str folder "/" year "/" str-month "/02_importAirport.csv")
+          sheetname "Sheet1"
+          _ (println (str "Converting file " in " into " out))
+          ]
+      (convert-pax-file in anac-spec out)
+      )))
 

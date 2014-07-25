c977ac400dbd5221051d073a2a97c3d0ba810fe2
Paul De Schacht <pauldeschacht@gmail.com>
2014-07-25 17:23:41 +0200

    added command line arguments and log4j dependencies

--- project.clj
+++ project.clj
@@ -7,7 +7,12 @@
                  [org.clojure/data.csv "0.1.2"]
                  [dk.ative/docjure "1.6.0"]
                  [clj-time "0.6.0"]
-                 ]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/tools.logging "0.3.0"]
+                 [log4j "1.2.15" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]]
   :jvm-opts ["-Xms4G" "-Xmx4G"]
   :main parser.main
   )

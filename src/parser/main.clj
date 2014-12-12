fffd057b6b16f9a21111f6e23410b29cd89bab12
Paul De Schacht <pauldeschacht@gmail.com>
2014-12-12 17:19:15 +0100

    removed hinting for the spec name, explicit spec is required now

--- src/parser/main.clj
+++ src/parser/main.clj
@@ -1,96 +1,140 @@
 (ns parser.main
   (:use [clojure.tools.cli :refer [parse-opts]])
-  (:use [parser.pax.core :as parser]
-        [parser.pax.dsl.aci :as aci]
-        [parser.pax.dsl.acsa :as acsa]
-        [parser.pax.dsl.albatross :as albatross]
-        [parser.pax.dsl.anac :as anac]
-        [parser.pax.dsl.anac-2014 :as anac-2014]
-        [parser.pax.dsl.btre :as btre]
-        [parser.pax.dsl.clac :as clac]
-        [parser.pax.dsl.destatis :as destatis]
-        [parser.pax.dsl.dhmi :as dhmi]
-        [parser.pax.dsl.icao-dataplus :as icao-dataplus]
-        [parser.pax.dsl.indonesia :as indonesia]
-        [parser.pax.dsl.kac :as kac]
-        [parser.pax.dsl.mx-sase :as mx]
-        [parser.pax.dsl.referential :as ref]
-        )
-    (:gen-class))
+  (:require [parser.pax.core :as parser]
+            [parser.pax.dsl.aci :as aci]
+            [parser.pax.dsl.acsa :as acsa]
+            [parser.pax.dsl.albatross :as albatross]
+            [parser.pax.dsl.anac :as anac]
+            [parser.pax.dsl.anac-2014 :as anac-2014]
+            [parser.pax.dsl.btre :as btre]
+            [parser.pax.dsl.bts-db1b :as bts]
+            [parser.pax.dsl.clac :as clac]
+            [parser.pax.dsl.destatis :as destatis]
+            [parser.pax.dsl.dhmi :as dhmi]
+            [parser.pax.dsl.icao-dataplus :as icao-dataplus]
+            [parser.pax.dsl.indonesia :as indonesia]
+            [parser.pax.dsl.kac :as kac]
+            [parser.pax.dsl.mx-sase :as mx]
+            [parser.pax.dsl.referential :as ref]
+            )
+  (:gen-class))
 
-(defn- substring*? [s sub]
-  (cond
-   (empty? s) false
-   (empty? sub) false
-   :else (not= (.indexOf s sub) -1)))
+(def spec-details
+  [{:code "ACI" :input "raw_aci_int.csv" :specname "aci-spec" :output "pax_aci_int.csv"}
+   {:code "ACI" :input "raw_aci_ww.csv" :specname "aci-spec" :output "pax_aci_int.csv"}
+   {:code "Albatross" :input "raw_albatross.csv" :specname "albatross-2008-spec" :output "pax_albatross_2008.csv"}
+   {:code "Albatross" :input "raw_albatross.csv" :specname "albatross-2009-spec" :output "pax_albatross_2009.csv"}
+   {:code "Albatross" :input "raw_albatross.csv" :specname "albatross-2010-spec" :output "pax_albatross_2010.csv"}
+   {:code "Albatross" :input "raw_albatross.csv" :specname "albatross-2011-spec" :output "pax_albatross_2011.csv"}
+   {:code "Albatross" :input "raw_albatross.csv" :specname "albatross-2012-spec" :output "pax_albatross_2012.csv"}
+   {:code "Albatross" :input "raw_albatross.csv" :specname "albatross-2013-spec" :output "pax_albatross_2013.csv"}
+   {:code "ANAC" :input "raw_anac.xlsx" :specname "anac-2014-spec" :output "pax_anac.csv" :sheet "(?i)passa"}
+   {:code "BTRE" :input "raw_btre_dom.xlsx" :specname "btre-dom-spec" :output "pax_btre_dom.csv" :sheet "Passengers"}
+   {:code "BTRE" :input "raw_btre_int.xlsx" :specname "btre-int-countrypairairline-spec" :output "pax_btre_int.csv" :sheet "Table_3"}
+   {:code "BTRE" :input "raw_btre_int.xlsx" :specname "btre-int-airportpair-spec" :output "pax_btre_int.csv" :sheet "Table_5"}
+   {:code "DESTATIS" :input "raw_destatis.xlsx" :specname "destatis-dom-spec" :output "pax_destatis_dom.csv" :sheet "2.2.1"}
+   {:code "DESTATIS" :input "raw_destatis.xlsx" :specname "destatis-int-dep-spec" :output "pax_destatis_int.csv" :sheet "2.3.1"}
+   {:code "DESTATIS" :input "raw_destatis.xlsx" :specname "destatis-int-arr-spec" :output "pax_destatis_int.csv" :sheet "2.3.2"}
+   {:code "DHMI" :input "raw_dhmi.xlsx" :specname "dhmi-spec" :output "pax_dhmi.csv" :sheet "(?i)yolcu"}
+   {:code "INDONESIA" :input "raw_indonesia.csv" :specname "indonesia-spec" :output "pax_indonesia.csv"}
+   {:code "MX" :input "raw_mx.xlsx" :specname "mx-spec" :output "pax_mx_dom.csv" :sheet "(?i)REG[ ]*NAC"}
+   {:code "MX" :input "raw_mx.xlsx" :specname "mx-spec" :output "pax_mx_int.csv" :sheet "(?i)REG[ ]*INT"}
+   {:code "MX" :input "raw_mx_airports.xlsx" :specname "mx-airport-spec" :output "pax_mx_airports.csv" }
+   {:code "KAC" :input "raw_kac_dom.csv" :specname "kac-spec" :output "pax_kac_dom.csv"}
+   {:code "KAC" :input "raw_kac_int.csv" :specname "kac-spec" :output "pax_kac_int.csv"}
+   {:code "POR" :input "ori_por_public.csv" :specname "ref-airport-spec" :output "airlines.csv"}
+   {:code "POR" :input "ori_por_public.csv" :specname "ref-city-spec" :output "cities.csv"}
+   {:code "POR" :input "ori_airlines.csv" :specname "ref-airline-spec" :output "airlines.csv"}
+   {:code "POR" :input "ori_countries.csv" :specname "ref-country-spec" :output "countries.csv"}
+   ])
 
-(defn- contains-all? [s subs]
-  (every? true? (map #(substring*? s %1) subs)))
 
-(defn- get-spec-for-source [in sheet hint]
-  (let [s (clojure.string/lower-case (str in " " sheet " " hint))]
-    (cond
-     (contains-all? s ["aci" "international"]) aci/aci-spec
-     (contains-all? s ["acsa"]) acsa/acsa-spec
-     (contains-all? s ["albatross" "2013!"]) albatross/albatross-spec-old-output-2013
-     (contains-all? s ["albatross" "2012!"]) albatross/albatross-spec-old-output-2012
-     (contains-all? s ["albatross" "2011!"]) albatross/albatross-spec-old-output-2011
-     (contains-all? s ["albatross" "2010!"]) albatross/albatross-spec-old-output-2010
-     (contains-all? s ["albatross" "2009!"]) albatross/albatross-spec-old-output-2009
-     (contains-all? s ["albatross" "2008!"]) albatross/albatross-spec-old-output-2008
-     (contains-all? s ["albatross" "2007!"]) albatross/albatross-spec-old-output-2007
-     (contains-all? s ["albatross"]) albatross/albatross-spec
-     ;;     (contains-all? s ["anac_brasil_2"]) anac/anac-spec
-     ;;     (contains-all? s ["anac_brasil_3"]) anac-2014/anac-2014-spec
-     ;;     (contains-all? s ["anac"]) anac-2014/anac-2014-spec
-     (contains-all? s ["anac"]) anac-2014/anac-2014-spec-convert-to-old
-     (contains-all? s ["btre" "domestic"]) btre/btre-dom-spec
-     (contains-all? s ["btre" "international" "Table_3"]) btre/btre-int-countrypairairline-spec
-     (contains-all? s ["btre" "international" "Table_5"]) btre/btre-int-airportpair-spec
-     (contains-all? s ["clac"]) clac/clac-spec
-     (contains-all? s ["destatis" "2.2.1"]) destatis/destatis-dom-spec
-     (contains-all? s ["destatis" "2.3.1"]) destatis/destatis-int-airportpair-spec
-     (contains-all? s ["destatis" "2.2.2"]) destatis/destatis-int-countryairport-spec
-     (contains-all? s ["dhmi"]) dhmi/dhmi-spec
-     (contains-all? s ["icao"]) icao-dataplus/icao-airport-spec
-     (contains-all? s ["indonesia"]) indonesia/indonesia-spec
-     (contains-all? s ["mx" "reg int"]) mx/mx-citypair-spec
-     (contains-all? s ["mx" "reg nac"]) mx/mx-citypair-spec
-     (contains-all? s ["mx" "regnal"]) mx/mx-citypair-spec
-     (contains-all? s ["mx" "regint"]) mx/mx-citypair-spec
-     (contains-all? s ["mx" "regnac"]) mx/mx-citypair-spec
-     (contains-all? s ["mx" "details"]) [(mx/mx-airport-spec "totdom" ["YEAR" "INTERNATIONAL"]) (mx/mx-airport-spec "totdom" ["YEAR" "DOMESTIC"]) ]
-     (contains-all? s ["kac"]) kac/kac-spec
-     (contains-all? s ["ori_por_public" "airport"]) ref/ref-airport-specs
-     (contains-all? s ["ori_por_public" "city"]) ref/ref-city-specs
-     (contains-all? in ["ori_airlines"]) ref/ref-airline-specs
-     (contains-all? in ["ori_countries"]) ref/ref-country-specs
-     :else (throw (Exception. "Cannot determine the specs for the input (forget the name of the Excel sheet?) ")))
-    ))
+(defn- get-spec [specname]
+  (case (clojure.string/lower-case specname)
+    "aci-spec" aci/aci-spec
+    "acsa" acsa/acsa-spec
+    "albatross-2007-spec" albatross/albatross-spec-old-output-2007
+    "albatross-2008-spec" albatross/albatross-spec-old-output-2008
+    "albatross-2009-spec" albatross/albatross-spec-old-output-2009
+    "albatross-2010-spec" albatross/albatross-spec-old-output-2010
+    "albatross-2011-spec" albatross/albatross-spec-old-output-2011
+    "albatross-2012-spec" albatross/albatross-spec-old-output-2012
+    "albatross-2013-spec" albatross/albatross-spec-old-output-2013
+    "anac-2014-spec" anac-2014/anac-2014-spec-convert-to-old
+    "btre-dom-spec" btre/btre-dom-spec
+    "btre-int-countrypairairline-spec" btre/btre-int-countrypairairline-spec
+    "btre-int-airportpair-spec" btre/btre-int-airportpair-spec
+    "bts-db1b-spec" bts/bts-db1b-spec
+    "bts-t100-spec" bts/bts-t100-spec
+    "clac-spec" clac/clac-spec
+    "destatis-dom-spec" destatis/destatis-dom-spec
+    "destatis-int-dep-spec" destatis/destatis-countryairport-dep-spec
+    "destatis-int-arr-spec" destatis/destatis-countryairport-arr-spec
+;;    "destatis-int-countryairport-spec" destatis/destatis-int-countryairport-spec
+    "dhmi-spec" dhmi/dhmi-spec
+    "icao-spec" icao-dataplus/icao-airport-spec
+    "indonesia-spec" indonesia/indonesia-spec
+    "mx-spec" mx/mx-citypair-spec
+    "mx-airport-spec" [(mx/mx-airport-spec "totdom" ["YEAR" "INTERNATIONAL"]) (mx/mx-airport-spec "totdom" ["YEAR" "DOMESTIC"]) ]
+    "kac-spec" [kac/kac-spec-arr-dom kac/kac-spec-arr-int kac/kac-spec-dep-dom kac/kac-spec-dep-int]
+    "ref-airport-spec" ref/ref-airport-specs
+    "ref-city-spec" ref/ref-city-specs
+    "ref-airline-spec" ref/ref-airline-specs
+    "ref-country-spec" ref/ref-country-specs
+    :else (throw (Exception. (str "Unknown spec name "  specname))))
+    )
 
-(defn process [in out sheet hint]
-  (let [spec (get-spec-for-source in sheet hint)]
-    (if (seq? spec)
-      (dorun (map #(convert-pax-file in %1 out sheet) spec))
-      (convert-pax-file in spec out sheet))))
+(defn find-file-in-folder [folder filename]
+  (let [f (clojure.java.io/file folder)
+        files (file-seq f)
+        ]
+    (->> files
+         (filter #(= filename (.getName %)))
+         (first)
+         )))
+
+
+(defn process [spec in out sheetname]
+  (if (coll? spec)
+    (doall (map #(parser/convert-pax-file in %1 out sheetname) spec))
+    (do
+      (println (str "convert-pax-file " in))
+      (parser/convert-pax-file in spec out sheetname)))
+    )
+
+(defn process-file [in-folder spec-detail]
+  (let [spec (get-spec (:specname spec-detail))]
+    (if-let [file (find-file-in-folder (str in-folder "/" (:code spec-detail)) (:input spec-detail))]
+      (let [folder (.getParent file)
+            in (str folder "/" (:input spec-detail))
+            out (str folder "/" (:output spec-detail))
+            ]
+        (process spec in out (:sheet spec-detail)))
+      (println (str "File " (:input spec-detail) " not found in folder " in-folder))
+      )
+    )
+  )
+
+(defn process-all [in-folder]
+  (println (str "process ALL " in-folder))
+  (doall (map #(process-file in-folder %) spec-details))
+  )
 
 (def cli-options
   [
-;;   ["-i" "--input" "input file name" :default "" ]
+   ["-c" "--spec SPEC" "specification name"]
    ["-i" "--input INPUT" "input file name"]
-   ["-s" "--sheet SHEET" "name of the sheet"]
    ["-o" "--output OUTPUT" "output file name"]
-   ["-h" "--hint HINT" "give hint for specifications"]
+   ["-s" "--sheet SHEET" "name of the sheet"]
    ])
 
 (defn -main[& args]
   (let [options (parse-opts args cli-options)
         in (:input (:options options))
-        sheet (:sheet (:options options))
+        specname (:spec (:options options))
         out (:output (:options options))
-        hint (:hint (:options options))
+        sheetname (:sheet (:options options))
         ]
-    (do
-      (println (str "Parsing " in " into " out))
-      (process in out sheet hint)
-      )))
+    (if (= "all" (clojure.string/lower-case specname))
+      (process-all in)
+      (process (get-spec specname) in out sheetname))))
